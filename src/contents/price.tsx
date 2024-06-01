d38da8bce67a18e708c511a0edb3ec790b364df9
losdu <s524256521@gmail.com>
2024-06-01 21:19:48 +0800

    milestone: 1

--- src/contents/price.tsx
+++ src/contents/price.tsx
@@ -12,17 +12,24 @@ import {
   createPublicClient,
   createWalletClient,
   custom,
+  encodePacked,
   formatEther,
   http,
+  keccak256,
   parseEther,
+  stringToBytes,
+  toBytes,
   toHex,
   verifyMessage,
   type Chain,
+  type PrivateKeyAccount,
   type WriteContractParameters
 } from "viem"
-import { optimism } from "viem/chains"
+import { generatePrivateKey, privateKeyToAccount } from "viem/accounts"
+import { optimism, sepolia } from "viem/chains"
 
 import { sendToBackground } from "@plasmohq/messaging"
+import { SecureStorage } from "@plasmohq/storage/secure"
 
 import { bodhiAbi } from "~core/bodhiAbi"
 
@@ -48,32 +55,111 @@ const publicClient = createPublicClient({
   transport: http()
 })
 
-const onRequestTxId = async (tweet) => {
-  const txId = await sendToBackground({
-    name: "getTxId",
-    body: {
-      tweet: tweet
-    },
-    extensionId: process.env.PLASMO_PUBLIC_EXTENSION_ID
-  })
-  return txId
+const storage = new SecureStorage()
+var account: PrivateKeyAccount
+
+const onRequestAccount = async () => {
+  const storage = new SecureStorage()
+  const savedAccount = await storage.get("account")
+  console.log("saved Account", savedAccount)
+  if (savedAccount) {
+    return savedAccount
+  }
+  // const privateKey = generatePrivateKey()
+  const privateKey = process.env.PLASMO_PUBLIC_PRIVATE_KEY
+
+  account = privateKeyToAccount(privateKey)
+
+  await storage.setPassword("123456") // The only diff
+
+  await storage.set("pk", privateKey)
+  await storage.set("account", account)
+  console.log("init account", account)
+  return account
 }
 
-const onRequestAssetId = async (txId: string) => {
-  const assetId = await sendToBackground({
-    name: "getAssetId",
-    body: {
-      txId: txId
-    },
-    extensionId: process.env.PLASMO_PUBLIC_EXTENSION_ID
+const onFirstCreate = async (tweet: Tweet) => {
+  if (!account) {
+    await onRequestAccount()
+  }
+  const walletClient = createWalletClient({
+    account,
+    chain: optimism as Chain,
+    transport: http()
   })
+
+  const createResult = await walletClient.writeContract({
+    address: process.env.PLASMO_PUBLIC_CONTRACT_ADDRESS,
+    abi: bodhiAbi,
+    functionName: "create",
+    args: [toHex(tweet.tweetURL)],
+    account: account
+  })
+  console.log("create Result", createResult)
+}
+
+const onBuyShare = async (asset: number, share: number, tweet: Tweet) => {
+  console.log("onBuyShare")
+  if (!account) {
+    await onRequestAccount()
+  }
+
+  const walletClient = createWalletClient({
+    account,
+    chain: optimism as Chain,
+    transport: http()
+  })
+
+  await walletClient.writeContract({
+    address: process.env.PLASMO_PUBLIC_CONTRACT_ADDRESS,
+    abi: bodhiAbi,
+    functionName: "buy",
+    args: [BigInt(asset), parseEther(share.toString())],
+    account: account
+  })
+
+  console.log("Buying shares")
+}
+
+const onSellShare = async (asset: number, share: number) => {
+  console.log("onSellShare")
+  if (!account) {
+    await onRequestAccount()
+  }
+  const walletClient = createWalletClient({
+    account,
+    chain: optimism as Chain,
+    transport: http()
+  })
+
+  await walletClient.writeContract({
+    address: process.env.PLASMO_PUBLIC_CONTRACT_ADDRESS,
+    abi: bodhiAbi,
+    functionName: "sell",
+    args: [BigInt(asset), parseEther(share.toString())],
+    account: account
+  })
+  console.log("Selling shares")
+}
+
+const getAssetId = async (tweetURL) => {
+  console.log("getBuyPrice")
+  const assetId = await publicClient.readContract({
+    address: process.env.PLASMO_PUBLIC_CONTRACT_ADDRESS,
+    abi: bodhiAbi,
+    functionName: "txToAssetId",
+    args: [keccak256(encodePacked(['string'],
+      [toHex(tweetURL)]
+    ))]
+  })
+  console.log("assetId",assetId)
   return assetId
 }
 
 const getBuyPrice = async (asset, share) => {
   console.log("getBuyPrice")
   const buyPrice = await publicClient.readContract({
-    address: toHex(process.env.PLASMO_PUBLIC_CONTRACT_ADDRESS),
+    address: process.env.PLASMO_PUBLIC_CONTRACT_ADDRESS,
     abi: bodhiAbi,
     functionName: "getBuyPrice",
     args: [BigInt(asset), parseEther(share.toString())]
@@ -84,20 +170,12 @@ const getBuyPrice = async (asset, share) => {
 const getPool = async (asset: number) => {
   console.log("getPool")
   const value = await publicClient.readContract({
-    address: toHex(process.env.PLASMO_PUBLIC_CONTRACT_ADDRESS),
+    address: process.env.PLASMO_PUBLIC_CONTRACT_ADDRESS,
     abi: bodhiAbi,
     functionName: "pool",
     args: [BigInt(asset)]
   })
   return formatEther(value)
-}
-
-const testAccount = async () => {
-  const account = await sendToBackground({
-    name: "testMainWorld",
-    extensionId: process.env.PLASMO_PUBLIC_EXTENSION_ID
-  })
-  return account
 }
 
 type Tweet = {
@@ -134,20 +212,22 @@ const Price: FC<PlasmoCSUIProps> = ({ anchor }) => {
   const [totalValue, setTotalValue] = useState("0")
   const [price, setPrice] = useState("0")
   const [tweet, setTweet] = useState<Tweet>()
+  const [isShowFirstBuy, setIsShowFirstBuy] = useState(false)
 
   useEffect(() => {
     const fetch = async () => {
       // Automatically update the price and total value when shares change
       const tweet = extractTweetData(anchor.element)
       setTweet(tweet)
-      const res = await onRequestTxId(tweet)
-
-      if (res.txId) {
-        const assetId = await onRequestAssetId(res.txId)
+      const assetId = await getAssetId(tweet.tweetURL)
+      console.log(assetId != 0n)
+      if (assetId != 0n) {
         const currentPrice = await getBuyPrice(assetId, share)
-        const pool = await getPool(asset)
+        const pool = await getPool(assetId)
         setPrice(currentPrice)
         setTotalValue(pool)
+      } else {
+        setIsShowFirstBuy(true)
       }
     }
 
@@ -156,38 +236,57 @@ const Price: FC<PlasmoCSUIProps> = ({ anchor }) => {
 
   return (
     <div className="flex flex-row items-center flex-1 gap-2 p-2 pl-4">
-      <div className="flex flex-row items-center gap-4">
-        <Input
-          value={share} // Convert number to string for the input field
-          onChange={(e) => setShare(Number(e.target.value))}
-          className="w-20 h-9"
-        />
+      <div className="flex flex-row gap-2 ">
         <Button
-          onClick={() => setShare((prevShare) => prevShare + 1)} // Correctly handle increment
           size="sm"
-          variant="outline">
-          <Plus className="text-xs font-thin" />
+          variant="outline"
+          onClick={() => onBuyShare(asset, share, tweet)}>
+          Buy
         </Button>
         <Button
-          onClick={async () => {
-            const account = await testAccount()
-            console.log("account", account)
-            return account
-          }}>
-          Test Account Link
+          size="sm"
+          variant="outline"
+          onClick={() => onSellShare(asset, share)}>
+          Sell
         </Button>
       </div>
-      <div className="flex flex-row justify-center flex-1 gap-4 ">
-        <div className="text-xl font-medium">
-          {parseFloat(totalValue).toFixed(4)}ETH
-        </div>
-        <div className="text-xs font-light">
-          <div>{parseFloat(price).toFixed(4)} ETH</div>
-          <div>
-            for {share} share{share !== 1 ? "s" : ""}
+      {isShowFirstBuy ? (
+        <Button
+          size="sm"
+          onClick={async () => {
+            onFirstCreate(tweet)
+            setIsShowFirstBuy(false)
+          }}>
+          Be the first Buyer
+        </Button>
+      ) : (
+        <div>
+          <div className="flex flex-row items-center gap-4">
+            <Input
+              value={share} // Convert number to string for the input field
+              onChange={(e) => setShare(Number(e.target.value))}
+              className="w-20 h-9"
+            />
+            <Button
+              onClick={() => setShare((prevShare) => prevShare + 1)} // Correctly handle increment
+              size="sm"
+              variant="outline">
+              <Plus className="text-xs font-thin" />
+            </Button>
+          </div>
+          <div className="flex flex-row justify-center flex-1 gap-4 ">
+            <div className="text-xl font-medium">
+              {parseFloat(totalValue).toFixed(4)}ETH
+            </div>
+            <div className="text-xs font-light">
+              <div>{parseFloat(price).toFixed(4)} ETH</div>
+              <div>
+                for {share} share{share !== 1 ? "s" : ""}
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      ) }
     </div>
   )
 }
